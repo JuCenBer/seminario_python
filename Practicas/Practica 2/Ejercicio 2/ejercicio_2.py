ba1ce34de450fe12f05ea5ad0e984e51f02c7b6d
JuCenBer <El_Jbc_99@hotmail.com>
2022-03-30 23:38:52 -0300

    terminado el ejercicio 2

--- Practicas/Practica 2/Ejercicio 2/ejercicio_2.py
+++ Practicas/Practica 2/Ejercicio 2/ejercicio_2.py
@@ -1,16 +1,29 @@
-texto = """The constants defined in this module are:The constants defined in␣
-,→this module are:
+from collections import Counter
+
+
+texto = """The constants defined in this module are:The constants defined in,
+this module are:
+
 string.ascii_letters
-The concatenation of the ascii_lowercase and ascii_uppercase constants␣
-,→described below. This value is not locale-dependent.
+The concatenation of the ascii_lowercase and ascii_uppercase constants,
+described below. This value is not locale-dependent.
+
 string.ascii_lowercase
-The lowercase letters 'abcdefghijklmnopqrstuvwxyz'. This value is not␣
-,→locale-dependent and will not change.
+The lowercase letters 'abcdefghijklmnopqrstuvwxyz'. This value is not,
+locale-dependent and will not change.
+
 string.ascii_uppercase
-The uppercase letters 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'. This value is not␣
-,→locale-dependent and will not change.
-"""
-# lista con todas las palabras
-print(texto.lower().split())
-# lista con palabras sin repetir
-print(set(texto.lower().split()))
+The uppercase letters 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'. This value is not,
+locale-dependent and will not change.""".lower().split()
+
+cnt = Counter()
+cnt1 = Counter(texto)
+
+for palabra in texto:
+    cnt[palabra] += 1
+print(cnt)
+
+print(cnt1)
+cont = Counter(texto).most_common(1)
+
+print(cont)
