3474bd4d939b5fa598b41403c7530933b05075a7
JuCenBer <El_Jbc_99@hotmail.com>
2022-04-11 23:51:43 -0300

    agregado notebook de jupyter para explicar el ejercicio 10

--- Practicas/Practica 2/Ejercicio 10/ejercicio_10.py
+++ Practicas/Practica 2/Ejercicio 10/ejercicio_10.py
@@ -1,10 +1,9 @@
 import os
 
-estudiantes = {}
-
 ruta = os.path.join(os.path.dirname(os.path.realpath(".")),
                     "Practica 2", "Ejercicio 10")
 
+estudiantes = {}
 archivo_eval1 = open(os.path.join(ruta, "eval1.txt"), 'r', encoding="UTF-8")
 archivo_eval2 = open(os.path.join(ruta, "eval2.txt"), "r", encoding="UTF-8")
 archivo_nombres1 = open(os.path.join(
@@ -13,8 +12,9 @@ archivo_nombres1 = open(os.path.join(
 nombres1 = archivo_nombres1.read().replace(
     "\n", "").replace("'", "").split(",")
 
-notas1 = archivo_eval1.read().replace("\n", "").replace(" ", "").split(",")
-notas2 = archivo_eval2.read().replace("\n", "").replace(" ", "").split(",")
+notas1 = archivo_eval1.read().replace("\n", "").strip(" ").split(",")
+notas2 = archivo_eval2.read().replace("\n", "").strip(" ").split(",")
+
 total = 0
 for estudiante in range(len(nombres1)):
     estudiantes[nombres1[estudiante]] = int(
@@ -23,10 +23,11 @@ for estudiante in range(len(nombres1)):
 
 prom = total/len(nombres1)
 print(f'La nota promedio del total de notas es: {prom}')
-print('Los alumnos que obtuvieron una nota total menor al promedio son: ')
-nombres = list(estudiantes.keys())
+print('Los alumnos que obtuvieron una nota total menor l promedio son: ')
+i = 0
 print('   Nombre\tNota')
-for estudiante in range(len(estudiantes)):
-    if(estudiantes[nombres[estudiante]] < prom):
+for estudiante in estudiantes:
+    if(estudiantes[estudiante] < prom):
         print(
-            f'{estudiante}  {nombres[estudiante]}\t{estudiantes[nombres[estudiante]]:<10}')
+            f'{i}  {estudiante}\t{estudiantes[estudiante]:<10}')
+        i += 1
